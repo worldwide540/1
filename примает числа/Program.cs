3fd8fb458aa60f532723d5e8fa88a6a2d1b490a7
narek <narek@DESKTOP-98JS365>
2024-06-20 10:42:35 +0300

    Добавьте файлы проекта.

---
+++ примает числа/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int number1 = 2;
+        int number2 = 9;
+        int sum = AddNumbers(number1, number2);
+        Console.WriteLine($"Сумма чисел {number1} и {number2} равна {sum}");
+    }
+
+    static int AddNumbers(int a, int b)
+    {
+        return a + b;
+    }
+}
